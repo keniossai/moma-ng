487b959ea588a1572e7dba25653571f622fef640
keniossai <kenossai1@gmail.com>
2023-04-04 13:00:55 +0100

    Create user table and added sweetalert

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,12 +16,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->unique();
+            $table->enum('gender', [User::MALE, User::FEMALE])->nullable();
+            $table->enum('state', [User::LAGOS, User::ABUJA])->nullable();
+            $table->string('photo')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('address')->nullable();
+            $table->enum('type',[User::ADMIN, User::MASSEUR, User::CLIENT])->default(User::CLIENT);
+            $table->integer('login_count')->default(0);
+            $table->timestamp('last_login')->nullable();
+            $table->timestamp('banned_until')->nullable();
+            $table->json('meta')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
